ee08df20d610d67d87eeb9be0f40425025229bba
csanrod <c.san.rodacc@gmail.com>
2025-10-18 22:36:59 +0200

    Added last details into documentation, to move throught v1.0.0

--- src/portfolio/__init__.py
+++ src/portfolio/__init__.py
@@ -4,4 +4,4 @@ This package implements a Retrieval-Augmented Generation system for
 querying professional portfolio information using natural language.
 """
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
